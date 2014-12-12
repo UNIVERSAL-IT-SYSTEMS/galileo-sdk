6cd72ec34d349933bb7a9154c3dcc7d7eb95e515
Zachary J. Fields <zachary_fields@yahoo.com>
2014-12-12 15:37:01 -0800

    Added tests for WCharacter.h - Updated APITest project of tests solution

--- tests/APITest/Main.cpp
+++ tests/APITest/Main.cpp
@@ -29,6 +29,67 @@ void setup()
     int x = 257;
     Log("CHECK: %s | ::constrain()\n", (constrain(constrain(x, 0, 256), 0, 255) == 255) ? "Pass" : "FAILED");
 
+    // Test ::isAlphaNumeric()
+    Log("CHECK: %s | ::isAlphaNumeric()\n", isAlphaNumeric('c') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isAlphaNumeric()\n", isAlphaNumeric('!') ? "FAILED" : "Pass");
+
+    // Test ::isAlpha()
+    Log("CHECK: %s | ::isAlpha()\n", isAlpha('c') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isAlpha()\n", isAlpha('7') ? "FAILED" : "Pass");
+
+    // Test ::isAscii()
+    Log("CHECK: %s | ::isAscii()\n", isAscii('!') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isAscii()\n", isAscii(0x0100) ? "FAILED" : "Pass");
+
+    // Test ::isWhitespace()
+    Log("CHECK: %s | ::isWhitespace()\n", isWhitespace('\t') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isWhitespace()\n", isWhitespace('c') ? "FAILED" : "Pass");
+
+    // Test ::isControl()
+    Log("CHECK: %s | ::isControl()\n", isControl('\n') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isControl()\n", isControl(' ') ? "FAILED" : "Pass");
+
+    // Test ::isDigit()
+    Log("CHECK: %s | ::isDigit()\n", isDigit('7') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isDigit()\n", isDigit('c') ? "FAILED" : "Pass");
+
+    // Test ::isGraph()
+    Log("CHECK: %s | ::isGraph()\n", isGraph('c') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isGraph()\n", isGraph(' ') ? "FAILED" : "Pass");
+
+    // Test ::isLowerCase()
+    Log("CHECK: %s | ::isLowerCase()\n", isLowerCase('c') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isLowerCase()\n", isLowerCase('C') ? "FAILED" : "Pass");
+
+    // Test ::isPrintable()
+    Log("CHECK: %s | ::isPrintable()\n", isPrintable('c') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isPrintable()\n", isPrintable('\n') ? "FAILED" : "Pass");
+
+    // Test ::isPunct()
+    Log("CHECK: %s | ::isPunct()\n", isPunct(',') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isPunct()\n", isPunct('c') ? "FAILED" : "Pass");
+
+    // Test ::isSpace()
+    Log("CHECK: %s | ::isSpace()\n", isSpace(' ') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isSpace()\n", isSpace('c') ? "FAILED" : "Pass");
+
+    // Test ::isUpperCase()
+    Log("CHECK: %s | ::isUpperCase()\n", isUpperCase('C') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isUpperCase()\n", isUpperCase('c') ? "FAILED" : "Pass");
+
+    // Test ::isHexadecimalDigit()
+    Log("CHECK: %s | ::isHexadecimalDigit()\n", isHexadecimalDigit('F') ? "Pass" : "FAILED");
+    Log("CHECK: %s | ::isHexadecimalDigit()\n", isHexadecimalDigit('G') ? "FAILED" : "Pass");
+
+    // Test ::toAscii()
+    //Log("CHECK: %s | ::toAscii()\n", toAscii('\n') ? "Pass" : "FAILED");
+
+    // Test ::toLowerCase()
+    Log("CHECK: %s | ::toLowerCase()\n", (toLowerCase('C') == 'c') ? "Pass" : "FAILED");
+
+    // Test ::toUpperCase()
+    Log("CHECK: %s | ::toUpperCase()\n", (toUpperCase('c') == 'C') ? "Pass" : "FAILED");
+
     // Call/test ...
     // next test here
 }
